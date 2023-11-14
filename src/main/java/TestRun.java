86c0302c31db2554675e570649ac8c9d13b035ff
“chj” <c15139530908@163.com>
2023-11-14 20:32:30 +0800

    jvm 学习

---
+++ src/main/java/TestRun.java
@@ -0,0 +1,14 @@
+/**
+ * @projectName: Jvm
+ * @package: PACKAGE_NAME
+ * @className: TestRun
+ * @author: chj
+ * @description:
+ * @date: Created in  2023/11/14 20:31
+ * @version: 1.0
+ */
+public class TestRun {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
